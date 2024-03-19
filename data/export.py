c5abfd739be83ea5916d48bc939008c007da70bf
nano-roboscan <hsoh@nanosys.kr>
2024-03-19 12:51:08 +0900

    fixed export parameter

--- data/export.py
+++ data/export.py
@@ -5,6 +5,6 @@ model = YOLO('yolov8n-pose.pt')  # load an official model
 #model = YOLO('yolov8n.pt')  # load an official model
 
 # Export the model
-#model.export(format='onnx', imgsz=640) # 640 x 640
-#model.export(format='onnx', imgsz=[640,480])  # 640 x 480
-model.export(format='onnx',imgsz=[640,640], opset=12)
+# Export the model
+model.export(format='onnx', imgsz=[480,640], opset=12)  # 640 x 480
+#model.export(format='onnx',imgsz=[640,640], opset=12)
